e9c4a4fa47cbe92bb2ce0ca4f5ac2ae182cecc9f
Zentenius <amaricross111@gmail.com>
2024-10-02 20:47:40 -0500

    logo and spline export update

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,7 @@ export default function Home() {
       />
        <nav className="absolute flex items-center top-0 w-full z-10">
         <div className="flex items-center justify-between p-6 w-full">
-          <div className='w-14 h-14 rounded-full bg-black'>
-
-          </div>
+          <img src='https://media.discordapp.net/attachments/895445208383250453/1291213708395151463/wWpexT9DtWMegAAAABJRU5ErkJggg.png?ex=66ff47f9&is=66fdf679&hm=213f4a991e06fc74b78efe9d3b0f3d620acb7c92d88220941b8bbeffc1011667&=&format=webp&quality=lossless&width=670&height=670' className='h-14 w-14'/>
           
           <div>
           <AlignRight />
